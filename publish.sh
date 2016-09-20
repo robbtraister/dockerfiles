69cf8c8761e616d12464e72c51e735e7d51c099d
Robb Traister <robb.traister@gmail.com>
2016-09-20 14:54:46 -0400

    improve publish logic

--- publish.sh
+++ publish.sh
@@ -4,15 +4,17 @@ for d in $(ls); do
   if [[ -d "$d" ]]; then
     (
       cd $d
-      if [[ -f "build.sh" ]]; then
-        ./build.sh && docker push robbtraister/$d
-      else
-        if [[ -f "docker-compose.yml" ]]; then
-          docker-compose build && docker push robbtraister/$d
+      (
+        if [[ -f "build.sh" ]]; then
+          ./build.sh
         else
-          docker build -t robbtraister/$d . && docker push robbtraister/$d
+          if [[ -f "docker-compose.yml" ]]; then
+            docker-compose build
+          else
+            docker build -t robbtraister/$d .
+          fi;
         fi;
-      fi;
+      ) && docker push robbtraister/$d
     )
   fi;
 done;
