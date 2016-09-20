4fb61977b06e18b3d3692d579bed3a318fcf877d
Robb Traister <robb.traister@gmail.com>
2016-09-20 10:22:07 -0400

    improve publish to support build.sh or docker-compose

--- publish.sh
+++ publish.sh
@@ -2,6 +2,17 @@
 
 for d in $(ls); do
   if [[ -d "$d" ]]; then
-    docker build -t robbtraister/$d $d && docker push robbtraister/$d
+    (
+      cd $d
+      if [[ -f "build.sh" ]]; then
+        ./build.sh && docker push robbtraister/$d
+      else
+        if [[ -f "docker-compose.yml" ]]; then
+          docker-compose build && docker push robbtraister/$d
+        else
+          docker build -t robbtraister/$d . && docker push robbtraister/$d
+        fi;
+      fi;
+    )
   fi;
 done;
