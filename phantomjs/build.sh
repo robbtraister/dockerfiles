f1adf30d3f534667d615d91eba1144096eceb41a
Robb Traister <robb.traister@gmail.com>
2016-09-20 14:33:35 -0400

    build intermediate image to be used for phantomjs_supervisor

--- phantomjs/build.sh
+++ phantomjs/build.sh
@@ -2,4 +2,7 @@ rm -r phantomjs.tar.gz
 docker build -t robbtraister/debian-phantomjs -f Dockerfile.debian . && \
 docker build -t phantomjs-dockerizer -f Dockerfile.dockerizer . && \
 docker run -ti -v `pwd`:/target phantomjs-dockerizer && \
+
+# build this base for phantomjs_supervisor
+docker build -t robbtraister/phantomjs-base -f Dockerfile.base .
 docker build -t robbtraister/phantomjs -f Dockerfile .
