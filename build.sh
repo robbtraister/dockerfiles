50bff13b672ea6bd926d77da64dfd9babb09235b
Robb Traister <robb.traister@gmail.com>
2016-09-26 11:19:19 -0400

    fix tagging

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -rf phantomjs.tar*
-docker build --rm=true -t robbtraister/debian-phantomjs -f Dockerfile.debian . && \
+docker build --rm=true -t debian-phantomjs -f Dockerfile.debian . && \
 docker build --rm=true -t phantomjs-dockerizer -f Dockerfile.dockerizer . && \
 docker run -ti -v `pwd`:/target phantomjs-dockerizer && \
-docker build --rm=true -t robbtraister/phantomjs .
+docker build --rm=true -t robbtraister/phantomjs-base .
