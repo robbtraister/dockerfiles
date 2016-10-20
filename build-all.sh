ca5454a6f84778545b808dea8b1defaf632f6ccb
Robb Traister <robb.traister@gmail.com>
2016-10-20 08:34:37 -0400

    remove dockerfiles from master

---
+++ build-all.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+docker pull alpine
+docker pull debian
+
+USERNAME=$(docker info 2> /dev/null | grep '^Username\: ' | sed -e 's/^Username\: *//')
+
+function rebuild() {
+  BRANCH=$1
+  git checkout $BRANCH
+  docker build -t $USERNAME/$BRANCH .
+}
+
+rebuild base
+rebuild watcher
+rebuild nginx
+rebuild nginx-supervisor
+rebuild nginx-watcher
+rebuild nodejs
+rebuild nodejs-supervisor
+rebuild nodejs-watcher
+rebuild phantomjs-base
+rebuild phantomjs
+rebuild phantomjs-supervisor
